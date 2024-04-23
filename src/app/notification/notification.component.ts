00d56fb1842f7f2abf623d731946dfc63dca73ad
sabski03 <saba27ge@gmail.com>
2024-04-23 22:21:47 +0400

    ngClass

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: ` <div class="alert alert-success" [hidden]="displayNotification">
+  template: ` <div class="alert alert-success" [ngClass]="{fadeOut: displayNotification}">
                 <p>this website uses cookies!</p>
                 <div class="close"><button class="btn" (click)="closeNotification()">X</button></div>
               </div>`,
-  styles: [".close{float: right; margin-top: -55px;}"]
+  styles: [".close{float: right; margin-top: -55px;}",
+           ".fadeOut {visibility: hidden; opacity: 0; transition: visibility 0s 2s, opacity 2s linear;}"]
 })
 export class NotificationComponent {
   displayNotification: boolean = false;

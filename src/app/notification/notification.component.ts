ce7f0df6933ea7a14b4ce30578e09a6caded713d
sabski03 <saba27ge@gmail.com>
2024-04-23 17:30:39 +0400

    angular basics

---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: ` <div class="alert alert-successg">
+                <p>this website uses cookies!</p>
+              </div>`,
+  styles: []
+})
+export class NotificationComponent {
+
+}
